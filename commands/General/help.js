2a94b910ee5ac219128d3812eba71bb9b35e5b44
Noah <66128227+ItsYourBoyNoah@users.noreply.github.com>
2020-07-10 16:12:11 +0300

    Update help.js

--- commands/General/help.js
+++ commands/General/help.js
@@ -17,7 +17,7 @@ class Help extends Command {
                 .setTitle(`Commands`)
                 .setThumbnail(this.client.user.displayAvatarURL({ format: "png" }))
                 .setDescription(
-                    `Use **${this.client.prefix}help <command>** for more info.`
+                    `Use **${this.client.prefix}help <command>** for more info. Made by xNo_ah#0001`
                 )
                 .addField(
                     `General [${
