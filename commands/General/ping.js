1b8471666f6157f81d27e8e5bfb8da905c769dc6
Noah <66128227+ItsYourBoyNoah@users.noreply.github.com>
2020-07-11 14:35:29 +0300

    Update ping.js

--- commands/General/ping.js
+++ commands/General/ping.js
@@ -11,17 +11,15 @@ class Ping extends Command {
     }
 
     async run(message, args, Discord) {
+        let botMsg = await message.channel.send("Pinging...")
+
         const embed = new Discord.MessageEmbed()
             .setTitle("Pong!")
             .setDescription(
-                `Websocket Latency: ${
-                this.client.ws.ping ? Math.floor(this.client.ws.ping) : 0
-                }ms\nBot Latency: ${Math.round(
-                    Date.now() - message.createdTimestamp
-                )}ms`
+                `Websocket Latency: ${ this.client.ws.ping ? Math.round(this.client.ws.ping) : 0 }ms\nBot Latency: ${botMsg.createdAt - message.createdAt}ms`
             )
             .setColor("#7289DA");
-        return message.channel.send(embed);
+        return botMsg.edit(embed).catch(() => botMsg.edit("An error occurred while pinging."));
     }
 }
 
